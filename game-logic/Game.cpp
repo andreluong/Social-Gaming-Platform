92535310015eff7e2a6f301b8d48fa33e72e85b2
ayi1 <ayi1@sfu.ca>
2024-09-27 10:55:30 -0700

    created skeleton for Game and RPS classes next will be testing them using gtest

--- game-logic/Game.cpp
+++ game-logic/Game.cpp
@@ -5,19 +5,53 @@ Game::Game(int id, const std::string& name, int maxP)
 
 Game::~Game() {}
 
-void Game::startGame() {}
+void Game::startGame() {
+    if (canStart()) {
+        status = "In Progress";
+        std::cout << "Game " << gameName << " has started!" << std::endl;
+    } else {
+        std::cout << "Cannot start the game, min 2 players required." << std::endl;
+    }
+}
 
-void Game::addPlayer(std::shared_ptr<Player> player) {}
+void Game::addPlayer(std::shared_ptr<Player> player) {
+    if (players.size() < maxPlayers) {
+        players.push_back(player);
+        std::cout << player->getName() << " has joined the game!" << std::endl;
+    } else {
+        std::cout << "Game is full, cannot add more players." << std::endl;
+    }
+}
 
-void Game::removePlayer(int playerID) {}
+void Game::removePlayer(int playerID) {
+    auto it = std::remove_if(players.begin(), players.end(),
+                             [playerID](const std::shared_ptr<Player>& p) {
+                                 return p->getID() == playerID;
+                             });
+    if (it != players.end()) {
+        std::cout << (*it)->getName() << " has left the game!" << std::endl;
+        players.erase(it, players.end());
+    }
+}
 
 bool Game::canStart() const {}
 
-void Game::endGame() {}
+void Game::endGame() {
+    status = "Completed";
+    std::cout << "Game " << gameName << " has ended!" << std::endl;
+}
 
-void Game::updateScoreBoard(const std::string& playerName, int score) {}
+void Game::updateScoreBoard(const std::string& playerName, int score) {
+    scoreBoard[playerName] += score;
+}
 
 std::string Game::getStatus() const {
     return status;
 }
-oid Game::displayScoreBoard() const {}
+oid Game::displayScoreBoard() const {
+    std::cout << "Scoreboard for game " << gameName << ":" << std::endl;
+    for (const auto& entry : scoreBoard) {
+        std::cout << entry.first << ": " << entry.second << std::endl;
+    }
+
+}
--- game-logic/include/Game.h
+++ game-logic/include/Game.h
@@ -6,16 +6,13 @@
 
 class Game {
     protected: //protected so we don't need getters/setter for our subclasses
-    //the game feilds, to be updated in the next iterations when requirements will be more clear
-    std::string gameName; //stores the name of the game          
-    int gameID;           // Unique id for the game
-
-    std::vector<std::shared_ptr<Player>> players; // a vector to store the list of players
-    // Scoreboard to track scores
+    std::string gameName;    
+    int gameID;          
+    std::vector<std::shared_ptr<Player>> players;
     std::unordered_map<std::string, int> scoreBoard; 
-    int maxPlayers;   // for max number of players
-    int currentRound;                 // to store the current round in the game
-    std::string status;               // the active status of the game
+    int maxPlayers;   
+    int currentRound;                 
+    std::string status;             
 
 public:
     // Constructor for the game class takes in unique id, the game name and the max number of players
