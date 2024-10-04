d53f8a699d7e5b50729dce727413ce4de614821d
Andre Luong <ala230@sfu.ca>
2024-10-04 01:28:46 -0700

    Start on control structures

---
+++ lib/utils/include/controlStructures.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include "dataVariant.h"
+#include "Server.h"
+#include <map>
+
+void forEach(
+    std::vector<dataVariant>& expression, 
+    std::vector<dataVariant>& statementList
+);
+
+void generalLoop(bool expression, std::vector<dataVariant>& statementList);
+
+void parallelForEach(
+    std::vector<dataVariant>& expression, 
+    std::vector<dataVariant>& statementList
+);
+
+void inParallel(std::vector<dataVariant>& statementList);
+
+void match(dataVariant expression, std::map<bool, std::vector<dataVariant>> expressionToStatementList);
