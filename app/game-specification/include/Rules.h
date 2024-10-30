7239ee07dcd30a566da65353f54ccb272d3e969c
andreluong <ala230@sfu.ca>
2024-10-30 15:17:44 -0700

    Add rule class

--- app/game-specification/include/Rules.h
+++ app/game-specification/include/Rules.h
@@ -1,17 +1,39 @@
-#ifndef RULES_H
-#define RULES_H
+#pragma once
 
 #include <string>
 #include <cpp-tree-sitter.h>
+#include "dataVariant.h"
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+#include <memory>
+
+class Rule {
+public:
+    Rule(std::string_view name) : name(name) {}
+
+    void addSpecification(std::string_view name, std::string_view value) {
+        specifications[name] = value;
+    }
+
+    std::unordered_map<std::string_view, ExpressionVariant> getSpecification() {
+        return specifications;
+    }
+
+private:
+    std::string_view name;
+    std::unordered_map<std::string_view, ExpressionVariant> specifications;
+};
 
 class Rules {
 public:
     Rules(const std::string& sourceCode);
 
-    void parseRule(const ts::Node& node) const;
-    void parseBody(ts::Node node) const;
+    void parseRule(const ts::Node& node);
+    void parseBody(ts::Node node);
+
+    void addRule(const std::string_view& ruleType, const ts::Node& ruleNode);
 private:
     std::string sourceCode;
+    std::vector<std::unique_ptr<Rule>> rules;
 };
-
-#endif
