951cd18ad68771bc1fb09db201b69f6c6ea1cb2d
ima23 <ima23@sfu.ca>
2024-10-31 16:25:53 +0000

    added initial header file

---
+++ app/game-specification/include/GameManager.h
@@ -0,0 +1,24 @@
+#include "GameSpecificationFactory.h"
+
+#ifndef GAMEMANAGER_H
+#define GAMEMANAGER_H
+
+class GameManager
+{
+private:
+    /* data */
+public:
+    GameManager(/* args */);
+    ~GameManager();
+};
+
+GameManager::GameManager(/* args */)
+{
+}
+
+GameManager::~GameManager()
+{
+}
+
+
+#endif
