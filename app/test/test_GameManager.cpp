1700ff63086e1482ba37fba6caffe139ee5c8592
ima23 <ima23@sfu.ca>
2024-11-02 02:05:29 +0000

    more initial code

---
+++ app/test/test_GameManager.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using namespace testing;
+
+class GameManagerTests : public testing::Test {
+protected:
+
+
+    void SetUp() override {
+
+    }
+};
+
+TEST_F(GameManagerTests, AddPlayer) {
+
+}
